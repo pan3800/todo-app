2f91768781ff32432b6c2b3aafb2b6dce3f5c660
Chae Seong Lim <pan1129@naver.com>
2023-10-24 08:02:45 +0900

    todo-app 기능 추가 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate";
@@ -18,12 +18,27 @@ const App = () => {
       id: 3,
       text: '일정 관리 앱 만들어 보기',
       checked: false,
-    }
-  ])
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
 
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos}/>
     </TodoTemplate>
   )
